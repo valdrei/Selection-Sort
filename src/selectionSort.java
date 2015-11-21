12946c62d3c03c681d482a0a37dd1a38d84219f7
feoliv <feoliv@IBM641-L1C9V49>
2015-11-21 18:38:38 -0200

    Adicionando Comentarios

--- src/selectionSort.java
+++ src/selectionSort.java
@@ -3,16 +3,24 @@
 public class selectionSort {
 
 	public static int[] fazerSelecao( int[] lista ){
+		
+		//Laço responsavel pela comparação entre os numeros
 		for (int i = 0; i < lista.length - 1; i++)
         {
             int index = i;
+            
+            //for responsavel por percorrer todo vetor comparando um numero
             for (int j = i + 1; j < lista.length; j++)
+            	//verifica se o um item do vetor é menor que o outro
                 if (lista[j] < lista[index])
                     index = j;
-      
+            //Adiciona o valor do indice com menor valor a respectiva variavel
             int menorValor = lista[index]; 
+            //Troca de posições
             lista[index] = lista[i];
+            //Troca de posições
             lista[i] = menorValor;
+            
         }
 
 		return lista;
@@ -20,13 +28,17 @@ public class selectionSort {
 	
 	public static void main(String[] args) {
 		
+		//Inicia uma lista desordenada
 		int[] lista1={18,1,6,8,4,10,2,3,7};
+		//Recebe o vetor ordenado
 		int[] lista2= fazerSelecao(lista1); 
 		
+		//Imprime o Vetor
 		for(int i:lista2){
 			 System.out.print(i);
 	         System.out.print(", ");
 	
 		}
 	}
+	
 }
